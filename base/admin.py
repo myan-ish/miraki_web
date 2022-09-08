2634266db293118649bf515fd5f2789d9ef4486d
Manish Kunwar <ranaxmond@gmail.com>
2022-09-08 20:57:08 +0545

    added image model and on admin page

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 
-from base.models import Category, Product, Style
+from base.models import Category, Image, Product, Style
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'style', 'price','image_tag')
+    list_filter = ('category', 'style')
+    search_fields = ('name', 'description')
+
+
 
 admin.site.register(Category)
 admin.site.register(Style)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Image)
--- base/models.py
+++ base/models.py
@@ -5,7 +5,7 @@ import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
-    slug = models.SlugField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True, blank=True)
 
     class Meta:
         ordering = ('name',)
@@ -21,7 +21,7 @@ class Category(models.Model):
 
 class Style(models.Model):
     name = models.CharField(max_length=50)
-    slug = models.SlugField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True, blank=True)
 
     class Meta:
         ordering = ('name',)
@@ -35,18 +35,40 @@ class Style(models.Model):
         self.slug = slugify.slugify(self.name)
         super(Style, self).save(*args, **kwargs)
 
+class Image(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='images/')
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'image'
+        verbose_name_plural = 'images'
+
+    def __str__(self):
+        return self.name
+    
+    def image_url(self):
+        return self.image.url
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    images = models.ManyToManyField('Image', related_name='products')
 
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     style = models.ForeignKey(Style, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ('name',)
         verbose_name = 'product'
         verbose_name_plural = 'products'
+    
+    def image_tag(self):
+        from django.utils.html import mark_safe
+        return mark_safe('<img src="/media/%s" width="300" height="150" />' % (self.images.all()[0].image))
+    
+    image_tag.short_description = 'Image'
 
     def __str__(self):
         return self.name
